ae4eb9a0a1d3a2bb633b6c611903d261a006959f
Elviskiprono <biggeelvo831@gmail.com>
2025-06-29 21:25:15 +0300

    Final setup

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./App.jsx",
+    "./components/**/*.{js,jsx}",
+    "./pages/**/*.{js,jsx}",
+    "./**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
